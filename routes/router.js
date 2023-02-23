7fb93e2bc33d224c8a54e728fe5db0366811d059
Royce Coviello <coviello.royce@gmail.com>
2023-02-22 22:57:02 -0800

    delete user added

--- routes/router.js
+++ routes/router.js
@@ -1,47 +1,60 @@
-const router = require('express').Router();
-const database = include('databaseConnection');
-const dbModel = include('databaseAccessLayer');
+const router = require("express").Router();
+const database = include("databaseConnection");
+const dbModel = include("databaseAccessLayer");
 //const dbModel = include('staticData');
 
-router.get('/', async (req, res) => {
-	console.log("page hit");
-	
-	try {
-		const result = await dbModel.getAllUsers();
-		res.render('index', {allUsers: result});
+router.get("/", async (req, res) => {
+  console.log("page hit");
 
-		//Output the results of the query to the Heroku Logs
-		console.log(result);
-	}
-	catch (err) {
-		res.render('error', {message: 'Error reading from MySQL'});
-		console.log("Error reading from mysql");
-	}
+  try {
+    const result = await dbModel.getAllUsers();
+    res.render("index", { allUsers: result });
+
+    //Output the results of the query to the Heroku Logs
+    console.log(result);
+  } catch (err) {
+    res.render("error", { message: "Error reading from MySQL" });
+    console.log("Error reading from mysql");
+  }
 });
 
-router.post('/addUser', async (req, res) => {
-	console.log("form submit");
-	console.log(req.body);
-	try {
-	const success = await dbModel.addUser(req.body);
-	if (success) {
-	res.redirect("/");
-	}
-	else {
-	res.render('error', {message: "Error writing to MySQL"});
-	console.log("Error writing to MySQL");
-	}
-	}
-	catch (err) {
-	res.render('error', {message: "Error writing to MySQL"});
-	console.log("Error writing to MySQL");
-	console.log(err);
-	}
-	});
+router.post("/addUser", async (req, res) => {
+  console.log("form submit");
+  console.log(req.body);
+  try {
+    const success = await dbModel.addUser(req.body);
+    if (success) {
+      res.redirect("/");
+    } else {
+      res.render("error", { message: "Error writing to MySQL" });
+      console.log("Error writing to MySQL");
+    }
+  } catch (err) {
+    res.render("error", { message: "Error writing to MySQL" });
+    console.log("Error writing to MySQL");
+    console.log(err);
+  }
+});
 
-router.post('/addUser', (req, res) => {
-	console.log("form submit");
-	console.log(req.body);
-	});
+router.post("/addUser", (req, res) => {
+  console.log("form submit");
+  console.log(req.body);
+});
+
+router.get("/deleteUser", async (req, res) => {
+  console.log("delete user");
+  console.log(req.query);
+  let userId = req.query.id;
+  if (userId) {
+    const success = await dbModel.deleteUser(userId);
+    if (success) {
+      res.redirect("/");
+    } else {
+      res.render("error", { message: "Error writing to MySQL" });
+      console.log("Error writing to mysql");
+      console.log(err);
+    }
+  }
+});
 
 module.exports = router;
